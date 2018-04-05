7d9797d9c1f35ec1acb195b9d9c67fd2904c96b2
ch46gc <chriseyhillgen@gmail.com>
2018-04-05 15:24:55 -0400

    Cleanup whitespace

--- app/src/main/java/com/example/android/scorekeeper/MainActivity.java
+++ app/src/main/java/com/example/android/scorekeeper/MainActivity.java
@@ -1,11 +1,7 @@
 package com.example.android.scorekeeper;
 
-
-
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import android.view.View;
 import android.widget.TextView;
 
@@ -18,8 +14,6 @@ public class MainActivity extends AppCompatActivity {
     TextView patriotsTeamScore;
     TextView teamEaglesName;
     TextView teamPatriotsName;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,95 +24,81 @@ public class MainActivity extends AppCompatActivity {
         patriotsTeamScore = findViewById(R.id.team_patriots_score);
         teamEaglesName = findViewById(R.id.team_eagles_name);
         teamPatriotsName = findViewById(R.id.team_patriots_name);
-
-
-    }
-
-    /**
-     * Increases the score  by 6.
-     */
+        }
+        /**
+         * Increases the score  by 6.
+         */
     public void addSixForTeamEagles(View v) {
         scoreTeamEagles = scoreTeamEagles + 6;
         displayForTeamEagles(scoreTeamEagles);
-    }
-
-
-    /**
-     * Increases the score for Team Eagles by 3.
-     */
+        }
+        /**
+         * Increases the score for Team Eagles by 3.
+         */
     public void addThreeForTeamEagles(View v) {
         scoreTeamEagles = scoreTeamEagles + 3;
         displayForTeamEagles(scoreTeamEagles);
-    }
-
-    /**
-     * Increases the score for Team Eagles by 2.
-     */
+        }
+        /**
+         * Increases the score for Team Eagles by 2.
+         */
     public void addTwoForTeamEagles(View v) {
         scoreTeamEagles = scoreTeamEagles + 2;
         displayForTeamEagles(scoreTeamEagles);
-    }
-
-    /**
-     * Increases the score for Team Eagles by 1.
-     */
+        }
+        /**
+         * Increases the score for Team Eagles by 1.
+         */
     public void addOneForTeamEagles(View v) {
         scoreTeamEagles = scoreTeamEagles + 1;
         displayForTeamEagles(scoreTeamEagles);
-    }
-
-    /**
-     * Increase the score for Team Patriots by 6.
-     */
+        }
+        /**
+         * Increase the score for Team Patriots by 6.
+         */
     public void addSixForTeamPatriots(View v) {
         scoreTeamPatriots = scoreTeamPatriots + 6;
         displayForTeamPatriots(scoreTeamPatriots);
-    }
-    /**
-     * Increase the score for Team Patriots by 3.
-     */
+        }
+       /**
+        * Increase the score for Team Patriots by 3.
+        */
     public void addThreeForTeamPatriots(View v) {
         scoreTeamPatriots = scoreTeamPatriots + 3;
         displayForTeamPatriots(scoreTeamPatriots);
-    }
-
-    /**
-     * Increase the score for Team Patriots by 2.
-     */
+       }
+       /**
+        * Increase the score for Team Patriots by 2.
+        */
     public void addTwoForTeamPatriots(View v) {
         scoreTeamPatriots = scoreTeamPatriots + 2;
         displayForTeamPatriots(scoreTeamPatriots);
-    }
-
-    /**
-     * Increase the score for Team Patriots by 1.
-     */
+       }
+       /**
+        * Increase the score for Team Patriots by 1.
+        */
     public void addOneForTeamPatriots(View v) {
         scoreTeamPatriots = scoreTeamPatriots + 1;
         displayForTeamPatriots(scoreTeamPatriots);
-    }
-
-    public void resetScore(View v) {
+       }
+       public void resetScore(View v) {
         scoreTeamEagles = 0;
         scoreTeamPatriots = 0;
         displayForTeamEagles(scoreTeamEagles);
         displayForTeamPatriots(scoreTeamPatriots);
-    }
-
-    /**
-     * Displays the given score for Team Patriots.
-     */
+       }
+       /**
+        * Displays the given score for Team Patriots.
+        */
     public void displayForTeamPatriots(int score) {
         TextView scoreView = findViewById(R.id.team_patriots_score);
         scoreView.setText(String.valueOf(score));
-    }
-
-    /**
-     * Displays the score Team Eagles
-     */
+       }
+       /**
+        * Displays the score Team Eagles
+        */
     private void displayForTeamEagles(int score) {
         TextView scoreView = findViewById(R.id.team_eagles_score);
         scoreView.setText(String.valueOf(score));
     }
-
 }
